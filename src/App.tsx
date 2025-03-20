cb36d42430fc278b9a4bba76159d4dcee3994665
weekiat-douze <lumweekiat98@gmail.com>
2025-03-20 21:28:56 +0800

    Process input field using tokenizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 // import kuromoji from '@aiktb/kuromoji'
 import KuromojiTokenizer from './tokenizer/KuromojiTokenizer.ts'
 import { useEffect } from 'react'
+import { Input } from "@/components/ui/input"
+
 
 function App() {
   // const [count, setCount] = useState(0)
-  useEffect(() => {
-    const asyncFunction = async () => {
+  const [text, setText] = useState("")
 
-      const tokenizer = await KuromojiTokenizer()
-      const nihongo = "わたしを雇ってください"
-      const path = tokenizer.tokenize(nihongo);
-      console.log(path)
-      console.log("HAR SEMO")
-    }
+  // useEffect(() => {
+  //   const asyncFunction = async () => {
 
-    asyncFunction()
+  //     const tokenizer = await KuromojiTokenizer()
+  //     const nihongo = "わたしを雇ってください"
+  //     const path = tokenizer.tokenize(nihongo);
+  //     // console.log(path)
+  //     // console.log("HAR SEMO")
+  //     setText(JSON.stringify(path))
+  //   }
+
+  //   asyncFunction()
 
 
-  }, [])
+  // }, [])
+
+  const inputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+
+
+    const tokenizer = await KuromojiTokenizer()
+    const nihongo = event.target.value;
+    // console.log("Nihongo", nihongo);
+    const path = tokenizer.tokenize(nihongo);
+    // console.log("Hello", path)
+    setText(JSON.stringify(path))
+  }
 
 
   return (
     <>
-      <div className="mx-auto max-w-fit">
-        Hello
+      <div className="flex flex-col mx-auto my-10 max-w-1/2">
+        <Input onChange={inputChange} />
+        <p>{text}</p>
+
+
       </div>
     </>
   )
