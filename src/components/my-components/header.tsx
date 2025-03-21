ceacbde99229afe77cadd90d4184d72ea94af07c
weekiat-douze <lumweekiat98@gmail.com>
2025-03-22 00:10:32 +0800

    Align header items

--- src/components/my-components/header.tsx
+++ src/components/my-components/header.tsx
@@ -5,8 +5,8 @@ import { BotMessageSquare, Github } from "lucide-react";
 
 export default function Header() {
     return <>
-        <div className="flex flex-col w-9/10 m-auto mt-14 gap-3">
-            <div className="flex justify-between">
+        <div className="flex flex-col w-9/10 m-auto mt-14 mb-14 gap-3">
+            <div className="flex justify-between items-center">
                 <h1 className="text-3xl font-notosans font-semibold">Japanese Search Demo</h1>
                 <a href="/" className="hover:text-slate-500">
                     <div className="flex gap-1">
