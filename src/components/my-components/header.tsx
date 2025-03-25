c0f93eb39d52033352cd354f341507685fda5533
weekiat-douze <lumweekiat98@gmail.com>
2025-03-25 20:28:53 +0800

    Add link to repo

--- src/components/my-components/header.tsx
+++ src/components/my-components/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="flex flex-col w-9/10 m-auto mt-14 mb-24 gap-3">
             <div className="flex justify-between items-center">
                 <h1 className=" text-lg sm:text-3xl font-notosans font-semibold">Japanese Search Demo</h1>
-                <a href="/" className="hover:text-slate-500">
+                <a href="/jpn-search-demo/" className="hover:text-slate-500">
                     <div className="flex gap-1">
                         <BotMessageSquare />
                         <p>Blog Post</p>
@@ -16,7 +16,7 @@ export default function Header() {
                 </a>
 
             </div>
-            <a href="/" className="hover:text-slate-500">
+            <a href="https://github.com/weekiat-douze/jpn-search-demo" className="hover:text-slate-500">
                 <div className="flex gap-1">
                     <Github />
                     <p className="font-ibmplexmono">jpn-search-demo</p>
